dfc185d6e7522673973c605e464bac79f43898f9
Faustino Correa <faustino.correa.m@gmail.com>
2022-06-01 10:25:45 -0400

    multiplicación regex

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ fn main() {
 
     // Regex
     let re_add = Regex::new(r"(\d+)\s?\+\s?(\d+)").unwrap();
+    let re_mult = Regex::new(r"(\d+)\s?\*\s?(\d+)").unwrap();
 
     //Traer datos del usuario
     println!("Por favor introduce tu expresión: ");
@@ -13,6 +14,25 @@ fn main() {
     stdin().read_line(&mut expression).unwrap();
 
     // Aplicar operaciones
+    //Multiplicación
+    loop {
+        let caps = re_mult.captures(expression.as_str());
+        
+        if caps.is_none() {
+            break;
+        }
+
+        let caps = caps.unwrap();
+
+        let cap_expression = caps.get(0).unwrap().as_str();
+        let left_value : i32 = caps.get(1).unwrap().as_str().parse().unwrap();
+        let right_value : i32 = caps.get(2).unwrap().as_str().parse().unwrap();
+
+        let mult = left_value * right_value;
+
+        expression = expression.replace(cap_expression, &mult.to_string());
+    }
+    
     //Suma
     loop {
         let caps = re_add.captures(expression.as_str());
